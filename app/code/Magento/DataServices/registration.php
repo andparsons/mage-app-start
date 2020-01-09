0fe47b119dfaba713962e3664bcd8c5b667aad75
Andrew Parsons <andrew@sozodesign.co.uk>
2020-01-09 11:33:06 +0000

    Remove data-services module, no sensai yet

--- app/code/Magento/DataServices/registration.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Magento_DataServices',
-    __DIR__
-);
--- app/code/Magento/DataServices/view/frontend/requirejs-config.js
+++
@@ -1,8 +0,0 @@
-var config = {
-    map: {
-        '*': {
-            dataServicesBase:     'https://commerce.adobedtm.com/v3/ds.min.js',
-            dataServicesDiscount: 'https://commerce.adobedtm.com/v3/discount.min.js'
-        }
-    }
-};
